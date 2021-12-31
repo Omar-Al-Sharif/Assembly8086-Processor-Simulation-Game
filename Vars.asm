bdf42bf14f1ab30169257d61eba334b7597d8ea4
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-12-31 19:25:47 +0200

    Update Vars.asm

--- Vars.asm
+++ Vars.asm
@@ -1,21 +1,27 @@
 
-axRegister dw 0
-
-bxRegister dw 0
-
-cxRegister dw 0
-
-dxRegister dw 0
-
-siRegister dw 0
-
-diRegister dw 0
-
-spRegister dw 0 
-
-bpRegister dw 0
-
-stackSegment db 16 dup(0)
+player1Registers label byte
+axPlayer1 dw 0
+bxPlayer1 dw 0
+cxPlayer1 dw 0
+dxPlayer1 dw 0
+siPlayer1 dw 0
+diPlayer1 dw 0
+spPlayer1 dw 0
+bpPlayer1 dw 0
+memoryPlayer1 db 16 dup(0)
+operation     DB         'MOV [AX],BX','$'
+instruction   DB         5 DUP('$')
+operands      DB         20 dup('$')
+operand1     DB          2 DUP('$')
+operand2     DB          2 DUP('$') 
+sizeOfInstruction  DW      ? 
+Operand db '[CX]'
+hasError db 0h
+hasBrack db 0h
+isNum db 0h
+isChar db 0h
+Counter db ?
+validAddressing db 0h
 
 player1Name db 11,?, 11 dup('$')
 
@@ -27,9 +33,4 @@ player2Score db ?
 
 instruction db 30,?, 30 dup('$')
 
-;Booleans:
 
-hasError db 0h
-hasBrack db 0h
-isNum db 0h
-isChar db 0h
