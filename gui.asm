aca8ee443049b03af2c4c7c8de22c0c110fa13be
Yalab7 <85079143+Yalab7@users.noreply.github.com>
2022-01-01 15:25:56 +0200

    Add files via upload

--- gui.asm
+++ gui.asm
@@ -28,10 +28,10 @@ displayRegister MACRO corX,corY,string,sizeOfString
 	MOV AH,2  ;; INT 10/AH=2 FOR SETTIING THE CURSOR
 	INT 10H
 	push dx
-	MOV AL,[SI]   ;;the ascii of the letter for al bec. the letter to print is stored in AL
-    MOV BL,0EH  ;; BL FOR THE COLOR 
-	MOV BH,0     ;; BH FOR THE PAGE NUMBER
-	MOV AH,0EH  ;; INT 10/AH=0EH FOR WRITING IN VGA MODE 
+	MOV AL,[SI]   	;;the ascii of the letter for al bec. the letter to print is stored in AL
+    MOV BL,0EH  	;; BL FOR THE COLOR 
+	MOV BH,0    	;; BH FOR THE PAGE NUMBER
+	MOV AH,0EH  	;; INT 10/AH=0EH FOR WRITING IN VGA MODE 
     INT 10H
 	pop dx
     
@@ -45,8 +45,8 @@ displayRegister MACRO corX,corY,string,sizeOfString
  
 
 ENDM
-;-------------------------------------------------------------------------
-Display MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE 
+
+DisplayWord MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE 
    local back 
    ;This is a macro to be used in displaying word in VGA mode
    ;; corX--> cordinate of x of the starting point of typing
@@ -55,25 +55,30 @@ Display MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE
    ;;numberByte--> the size of the string
    ;;COLOR-->to select the color of the printed word or byte
    ;;ASCII_TABLE for XLAT instruction
+
+    XOR AX,AX
+    XOR BX,BX
+    XOR CX,CX
+    XOR DX,DX
    
     MOV SI,OFFSET STRING
     MOV CL,4 
     MOV DH,corY
     MOV DL,corX
     MOV CL,4
-    MOV DI, numberByte    
+    MOV DI, numberByte
     
     INC SI  ;; this is to start by the first of 
     MOV AL,[SI]
   BACK:
-    ;the next section for trying to isolate the nipple to print it only 
-	 sub ax,ax
+    ;the next section for trying to isolate the nibble to print it only 
+	sub ax,ax
 	MOV AL,[SI]
 	ROl Ax,CL
 	ROR AL,CL
 	PUSH AX
 	MOV AL,AH
-	MOV BX,OFFSET ASCII_TABLE
+	MOV BX,OFFSET ASCII_TABLE 
 	XLAT
 	
 	;NOW LET'S SET THE CURSOR 
@@ -82,7 +87,7 @@ Display MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE
 	MOV AH,2  ;; INT 10/AH=2 FOR SETTIING THE CURSOR
 	INT 10H
 	
-	;THIS FOR TYPING THE NIPPLE
+	;THIS FOR TYPING THE nibble
 	
 	MOV BL,COLOR  ;; BL FOR THE COLOR 
 	MOV BH,0  ;; BH FOR THE PAGE NUMBER
@@ -96,8 +101,9 @@ Display MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE
 	INT 10H
 	
 	
-	;NOW WE WILL PRINT THE SECOND NIPPLE
+	;NOW WE WILL PRINT THE SECOND NIBBLE
 	POP AX
+	MOV BX,OFFSET ASCII_TABLE
 	XLAT
 	
 	MOV BL,COLOR  ;; BL FOR THE COLOR 
@@ -114,8 +120,9 @@ Display MACRO corX,corY,string,numberByte,COLOR,ASCII_TABLE
 	JNE BACK 
 	
    
-ENDM
-;---------------------------------------------------------
+  ENDM
+;-------------------------------------------------------------------------
+
 .Model Small
 .Stack 64
 .Data
@@ -123,6 +130,22 @@ ENDM
 ;corY db ?
 ;sizeOfString db 2
 
+memoryPlayer1 db 16 dup(0)
+memoryPlayer2 db 17 dup(0)
+
+scoreLabel	 db 'Score:'
+scorePlayer1 db 50h
+scorePlayer2 db 70h
+namePlayer1 db 'Omar'
+sizeNamePlayer1 db ?
+namePlayer2 db 'Atef'
+sizeNamePlayer2 db ?
+
+cfPlayer1 db 0h
+cfPlayer22 db 00h
+cfPlayer2 db 0h
+
+cfLabel db 'CF'
 axLabel db 'AX'
 bxLabel db 'BX'
 cxLabel db 'CX'
@@ -133,15 +156,25 @@ diLabel db 'DI'
 spLabel db 'SP'
 bpLabel db 'BP'
 
-axnum db '0000'
-bxnum db '0000'
-cxnum db '0000'
-dxnum db '0000'
+axPlayer1 DW 0000h
+bxPlayer1 DW 0001h
+cxPlayer1 DW 0002h
+dxPlayer1 DW 0003h
 
-sinum db '0000'
-dinum db '0000'
-spnum db '0000'
-bpnum db '0000'
+siPlayer1 DW 0004h
+diPlayer1 DW 0005h
+spPlayer1 DW 0006h
+bpPlayer1 DW 0007h
+
+axPlayer2 DW 0008h
+bxPlayer2 DW 0009h
+cxPlayer2 DW 000Ah
+dxPlayer2 DW 000Bh
+
+siPlayer2 DW 000Ch
+diPlayer2 DW 000Dh
+spPlayer2 DW 000Eh
+bpPlayer2 DW 000Fh
 Palette LABEL WORD
 	dw 0000h;0  -GRB ChatLine
 	dw 00A1h;1  -GRB Strokes
@@ -166,6 +199,8 @@ Palette LABEL WORD
 ImageWidth EQU 320
 ImageHeight EQU 200
 
+ASC_TABL DB '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+BOL_TABL DB '0','1'
 Filename DB 'guifinal.bin', 0
 
 Filehandle DW ?
@@ -177,6 +212,7 @@ ImageData DB ImageWidth*ImageHeight dup(0)
 MAIN PROC FAR
     MOV AX , @DATA
     MOV DS , AX
+	XOR AX,AX ;clear ax
     ;Change the current mode to graphics mode (VGA 320X200 PIXEL)
     MOV AH, 0   
     MOV AL, 13h 
@@ -221,92 +257,139 @@ JNE drawLoop
 	XOR CX,CX ;clear cx
 	XOR DX,DX ;clear dx
 	DisplayRegister 0,3,axLabel,2
-	;Call DisplayContents
-	
 	DisplayRegister 0,5,bxLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 0,7,cxLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 0,9,dxLabel,2
-	;Call DisplayContents
-	
-
-
 	DisplayRegister 14,3,siLabel,2
-	;Call DisplayContents
-	
 	DisplayRegister 14,5,diLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 14,7,spLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 14,9,bpLabel,2
-	;Call DisplayContents
-
-
-
 	DisplayRegister 21,3,axLabel,2
-	;Call DisplayContents
-	
 	DisplayRegister 21,5,bxLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 21,7,cxLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 21,9,dxLabel,2
-	;Call DisplayContents
-
-
 	DisplayRegister 34,3,siLabel,2
-	;Call DisplayContents
-	
 	DisplayRegister 34,5,diLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 34,7,spLabel,2
-	;Call DisplayContents
-
 	DisplayRegister 34,9,bpLabel,2
-	;Call DisplayContents
 
-	DisplayRegister 3,3,axnum,4
+	DisplayRegister 20,21,scoreLabel,6
+	DisplayRegister 0,21,scoreLabel,6
 
-	DisplayRegister 3,5,bxnum,4
+;;;;;;;;;;;;;;;;;;;;;;;Name;;;;;;;;;;;;;;;;;;;;;
 
-	DisplayRegister 3,7,cxnum,4
+	DisplayRegister 3,11,namePlayer1,7
+	DisplayRegister 23,11,namePlayer2,7
 
-	DisplayRegister 3,9,dxnum,4
+;;;;;;;;;;Setting register values;;;;;;;;;;;;;
 
-
-	DisplayRegister 9,3,sinum,4
-
-	DisplayRegister 9,5,dinum,4
-
-	DisplayRegister 9,7,spnum,4
-
-	DisplayRegister 9,9,bpnum,4
-
-
-	DisplayRegister 24,3,axnum,4
-
-	DisplayRegister 24,5,bxnum,4
-
-	DisplayRegister 24,7,cxnum,4
-
-	DisplayRegister 24,9,dxnum,4
+	DisplayWord 3,3,axPlayer1,2,09h,ASC_TABL
+	DisplayWord 3,5,bxPlayer1,2,09h,ASC_TABL
+	DisplayWord 3,7,cxPlayer1,2,09h,ASC_TABL
+	DisplayWord 3,9,dxPlayer1,2,09h,ASC_TABL
+	DisplayWord 9,3,siPlayer1,2,09h,ASC_TABL
+	DisplayWord 9,5,diPlayer1,2,09h,ASC_TABL
+	DisplayWord 9,7,spPlayer1,2,09h,ASC_TABL
+	DisplayWord 9,9,bpPlayer1,2,09h,ASC_TABL
 
 
-	DisplayRegister 29,3,sinum,4
+	DisplayWord 24,3,axPlayer2,2,09h,ASC_TABL
+	DisplayWord 24,5,bxPlayer2,2,09h,ASC_TABL
+	DisplayWord 24,7,cxPlayer2,2,09h,ASC_TABL
+	DisplayWord 24,9,dxPlayer2,2,09h,ASC_TABL
+	DisplayWord 29,3,siPlayer2,2,09h,ASC_TABL
+	DisplayWord 29,5,diPlayer2,2,09h,ASC_TABL
+	DisplayWord 29,7,spPlayer2,2,09h,ASC_TABL
+	DisplayWord 29,9,bpPlayer2,2,09h,ASC_TABL
 
-	DisplayRegister 29,5,dinum,4
+;;;;;;;;;;;;;;;;;;;;;;;Score;;;;;;;;;;;;;;;;;;;;;
 
-	DisplayRegister 29,7,spnum,4
+	DisplayWord 6,21,scorePlayer1,1,09h,ASC_TABL	
+	DisplayWord 26,21,scorePlayer2,1,09h,ASC_TABL
 
-	DisplayRegister 29,9,bpnum,4
+;;;;;;;;;;;;;;;;;;;;;;;Name;;;;;;;;;;;;;;;;;;;;;
+	DisplayRegister 3,11,namePlayer1,7
+	DisplayRegister 23,11,namePlayer2,7
+;;;;;;;;;;;;;;;;;;;;;;;MemoryPlayer1;;;;;;;;;;;;;;;;;;;;;
+
+	DisplayWord 16,0,memoryPlayer1,1,09h,ASC_TABL
+	DisplayWord 16,1,memoryPlayer1+1,1,09h,ASC_TABL
+	DisplayWord 16,2,memoryPlayer1+2,1,09h,ASC_TABL
+	DisplayWord 16,3,memoryPlayer1+3,1,09h,ASC_TABL
+	DisplayWord 16,4,memoryPlayer1+4,1,09h,ASC_TABL
+	DisplayWord 16,5,memoryPlayer1+5,1,09h,ASC_TABL
+	DisplayWord 16,6,memoryPlayer1+6,1,09h,ASC_TABL
+	DisplayWord 16,7,memoryPlayer1+7,1,09h,ASC_TABL
+	DisplayWord 16,8,memoryPlayer1+8,1,09h,ASC_TABL
+	DisplayWord 16,9,memoryPlayer1+9,1,09h,ASC_TABL
+	DisplayWord 16,10,memoryPlayer1+10,1,09h,ASC_TABL
+	DisplayWord 16,11,memoryPlayer1+11,1,09h,ASC_TABL
+	DisplayWord 16,12,memoryPlayer1+12,1,09h,ASC_TABL
+	DisplayWord 16,13,memoryPlayer1+13,1,09h,ASC_TABL
+	DisplayWord 16,14,memoryPlayer1+14,1,09h,ASC_TABL
+	DisplayWord 16,15,memoryPlayer1+15,1,09h,ASC_TABL
+
+	DisplayWord 16,17,cfPlayer1,1,0Fh,BOL_TABL
+
+	DisplayRegister 19,0,ASC_TABL+0,1
+	DisplayRegister 19,1,ASC_TABL+1,1
+	DisplayRegister 19,2,ASC_TABL+2,1
+	DisplayRegister 19,3,ASC_TABL+3,1
+	DisplayRegister 19,4,ASC_TABL+4,1
+	DisplayRegister 19,5,ASC_TABL+5,1
+	DisplayRegister 19,6,ASC_TABL+6,1
+	DisplayRegister 19,7,ASC_TABL+7,1
+	DisplayRegister 19,8,ASC_TABL+8,1
+	DisplayRegister 19,9,ASC_TABL+9,1
+	DisplayRegister 19,10,ASC_TABL+10,1
+	DisplayRegister 19,11,ASC_TABL+11,1
+	DisplayRegister 19,12,ASC_TABL+12,1
+	DisplayRegister 19,13,ASC_TABL+13,1
+	DisplayRegister 19,14,ASC_TABL+14,1
+	DisplayRegister 19,15,ASC_TABL+15,1
+
+	DisplayRegister 18,17,cfLabel,2
+
+
+;;;;;;;;;;;;;;;;;;;;;;;MemoryPlayer2;;;;;;;;;;;;;;;;;;;;;
+
+	DisplayWord 36,0,memoryPlayer2,1,09h,ASC_TABL
+	DisplayWord 36,1,memoryPlayer2+1,1,09h,ASC_TABL
+	DisplayWord 36,2,memoryPlayer2+2,1,09h,ASC_TABL
+	DisplayWord 36,3,memoryPlayer2+3,1,09h,ASC_TABL
+	DisplayWord 36,4,memoryPlayer2+4,1,09h,ASC_TABL
+	DisplayWord 36,5,memoryPlayer2+5,1,09h,ASC_TABL
+	DisplayWord 36,6,memoryPlayer2+6,1,09h,ASC_TABL
+	DisplayWord 36,7,memoryPlayer2+7,1,09h,ASC_TABL
+	DisplayWord 36,8,memoryPlayer2+8,1,09h,ASC_TABL
+	DisplayWord 36,9,memoryPlayer2+9,1,09h,ASC_TABL
+	DisplayWord 36,10,memoryPlayer2+10,1,09h,ASC_TABL
+	DisplayWord 36,11,memoryPlayer2+11,1,09h,ASC_TABL
+	DisplayWord 36,12,memoryPlayer2+12,1,09h,ASC_TABL
+	DisplayWord 36,13,memoryPlayer2+13,1,09h,ASC_TABL
+	DisplayWord 36,14,memoryPlayer2+14,1,09h,ASC_TABL
+	DisplayWord 36,15,memoryPlayer2+15,1,09h,ASC_TABL
+
+	DisplayWord 36,17,cfPlayer22,1,0Fh,BOL_TABL
+
+	DisplayRegister 39,0,ASC_TABL+0,1
+	DisplayRegister 39,1,ASC_TABL+1,1
+	DisplayRegister 39,2,ASC_TABL+2,1
+	DisplayRegister 39,3,ASC_TABL+3,1
+	DisplayRegister 39,4,ASC_TABL+4,1
+	DisplayRegister 39,5,ASC_TABL+5,1
+	DisplayRegister 39,6,ASC_TABL+6,1
+	DisplayRegister 39,7,ASC_TABL+7,1
+	DisplayRegister 39,8,ASC_TABL+8,1
+	DisplayRegister 39,9,ASC_TABL+9,1
+	DisplayRegister 39,10,ASC_TABL+10,1
+	DisplayRegister 39,11,ASC_TABL+11,1
+	DisplayRegister 39,12,ASC_TABL+12,1
+	DisplayRegister 39,13,ASC_TABL+13,1
+	DisplayRegister 39,14,ASC_TABL+14,1
+	DisplayRegister 39,15,ASC_TABL+15,1
+
+	DisplayRegister 38,17,cfLabel,2
 
 	call CloseFile
     
